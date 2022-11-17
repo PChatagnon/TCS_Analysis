ad964e63862ff339caccd25eab99e49ce0b44dc5
Pierre Chatagnon <pierrec@jlab.org>
2022-11-17 10:48:12 -0500

    Double electron analysis code

---
+++ analysisTCS_EE.C
@@ -0,0 +1,188 @@
+#include "TFile.h"
+#include "TTree.h"
+#include "TChain.h"
+#include "TH1F.h"
+#include "TF1.h"
+#include "TTreeReader.h"
+#include "TTreeReaderValue.h"
+#include "TTreeReaderArray.h"
+#include "TH2D.h"
+#include "TLorentzVector.h"
+#include "TMath.h"
+#include "TCanvas.h"
+#include "TH3F.h"
+#include "bib/TCSclass.h"
+#include "bib/TCSfunc.h"
+#include "bib/TCSPlotClass.h"
+#include "bib/TCSMomentumCorrection.h"
+#include "bib/TCSParameters.h"
+#include "bib/TCSAcceptanceClass.h"
+#include "bib/TCSPositronClass.h"
+#include "bib/TCSFiducialCuts.h"
+#include "bib/TCSBinVolumeCorrection.h"
+#include "bib/TCSBSAclass.h"
+#include "bib/TCSAFBclass.h"
+#include "bib/TCSRRatioclass.h"
+#include "bib/TCSEvent.h"
+#include "bib/TCSMCEvent.h"
+#include "bib/TCSBGEvent.h"
+#include "bib/TCSRunSelector.h"
+
+#include "reader.h"
+
+#include <ctime> // time_t
+#include <cstdio>
+using namespace std;
+
+#define ADDVAR(x, name, t, tree) tree->Branch(name, x, TString(name) + TString(t))
+
+int analysisTCS_EE()
+{
+
+	time_t begin, intermediate, end;
+
+	time(&begin);
+
+	gROOT->SetBatch(kTRUE);
+	gStyle->SetOptStat(111);
+	gStyle->SetPalette(55);
+	gStyle->SetLabelSize(.05, "xyz");
+	gStyle->SetTitleSize(.05, "xyz");
+	gStyle->SetTitleSize(.07, "t");
+	gStyle->SetMarkerStyle(13);
+	gStyle->SetOptFit(1);
+
+	Int_t argc = gApplication->Argc();
+	char **argv = gApplication->Argv();
+
+	TString output_file = (TString)(argv[argc - 1]);
+	TFile *outFile = new TFile(Form("outputTCS_%s.root", output_file.Data()), "recreate");
+	TTree *outT = new TTree("tree", "tree");
+
+	TLorentzVector tree_Electron_1, tree_Electron_2, tree_Proton;
+	outT->Branch("Electron_1", "TLorentzVector", &tree_Electron_1);
+	outT->Branch("Electron_2", "TLorentzVector", &tree_Electron_2);
+	outT->Branch("Proton", "TLorentzVector", &tree_Proton);
+
+
+	TString fvars[] = {
+		"Pt_Frac", "MMass", "electron_1_SF", "electron_2_SF", "electron_1_Nphe", "electron_2_Nphe",  "status_electron_1",  "status_electron_2", "status_proton"};
+
+	std::map<TString, Float_t>
+		outVars;
+	for (size_t i = 0; i < sizeof(fvars) / sizeof(TString); i++)
+	{
+		outVars[fvars[i]] = 0.;
+		ADDVAR(&(outVars[fvars[i]]), fvars[i], "/F", outT);
+	}
+
+	TString nameFiles = "";
+	int nbEvent = 0;
+	////////////////////////////////////////////
+	// Get file name
+	////////////////////////////////////////////
+	for (Int_t i = 3; i < (argc - 1); i++)
+	{
+
+		if (TString(argv[i]).Contains(".hipo"))
+		{
+			nameFiles = TString(argv[i]);
+		}
+		else continue;
+
+		////////////////////////////////////////////
+		// hipo reader
+		hipo::reader reader;
+		hipo::dictionary factory;
+		hipo::event hipo_event;
+		////////////////////////////////////////////
+
+		reader.open(nameFiles);
+		reader.readDictionary(factory);
+		factory.show();
+
+		hipo::bank EVENT(factory.getSchema("REC::Event"));
+		hipo::bank PART(factory.getSchema("REC::Particle"));
+		hipo::bank SCIN(factory.getSchema("REC::Scintillator"));
+		hipo::bank CHE(factory.getSchema("REC::Cherenkov"));
+		hipo::bank CALO(factory.getSchema("REC::Calorimeter"));
+		hipo::bank RUN(factory.getSchema("RUN::config"));
+		hipo::bank MCPART(factory.getSchema("MC::Particle"));
+		hipo::bank MCEVENT(factory.getSchema("MC::Event"));
+		hipo::bank TRACK(factory.getSchema("REC::Track"));
+		hipo::bank TRAJ(factory.getSchema("REC::Traj"));
+
+		outFile->cd();
+
+		
+		while (reader.next())
+		{
+			nbEvent++;
+			if (nbEvent % 500000 == 0)
+			{
+				time(&intermediate);
+				double intermediate_time = difftime(intermediate, begin);
+
+				cout << nbEvent << " events processed in " << intermediate_time << "s"
+					 << "\n";
+			}
+
+			EEEvent ev;
+
+			reader.read(hipo_event);
+			hipo_event.getStructure(MCPART);
+			hipo_event.getStructure(MCEVENT);
+			hipo_event.getStructure(RUN);
+			hipo_event.getStructure(PART);
+			hipo_event.getStructure(SCIN);
+			hipo_event.getStructure(CHE);
+			hipo_event.getStructure(CALO);
+			hipo_event.getStructure(EVENT);
+			hipo_event.getStructure(TRAJ);
+			hipo_event.getStructure(TRACK);
+
+			if (PART.getSize() < 1)
+				continue;
+
+			ev.Set_Particles(PART);
+
+			if (ev.recep == 1 && ev.recem == 2  )
+			{
+
+				// ev.Apply_EC_Cuts(CALO);
+				ev.Associate_detector_resp(CHE, SCIN, CALO);
+				ev.Set_Nphe_HTCC();
+				ev.Compute_SF();
+
+				outVars["Pt_Frac"] = (ev.vBeam + ev.vRestProton - ev.Electron[0].Vector - ev.Electron[1].Vector - ev.Proton.Vector).Pt();
+				outVars["MMass"] = (ev.vBeam + ev.vRestProton - ev.Electron[0].Vector - ev.Electron[1].Vector - ev.Proton.Vector).M2();
+				outVars["electron_1_SF"] = ev.electron_1_SF;
+				outVars["electron_2_SF"] = ev.electron_1_SF;
+				outVars["electron_1_Nphe"] = ev.electron_Nphe;
+				outVars["electron_2_Nphe"] = ev.electron_Nphe;
+				outVars["status_electron_1"] = ev.Electron[0].status;
+				outVars["status_electron_2"] = ev.Electron[1].status;
+				outVars["status_proton"] = ev.Proton.status;
+
+				tree_Electron_1 = ev.Electron[0].Vector;
+				tree_Electron_2 = ev.Electron[1].Vector;
+				tree_Proton = ev.Proton.Vector;
+
+				outT->Fill();
+			}
+		}
+	}
+
+	outT->Write();
+	outFile->Write();
+	outFile->Close();
+
+	time(&end);
+
+	double difference = difftime(end, begin);
+	printf("All this work done in only %.2lf seconds. Congratulations !\n", difference);
+
+	gApplication->Terminate();
+
+	return 0;
+}
---
+++ bib/TCSEEEvent.h
@@ -0,0 +1,259 @@
+#ifndef TCSEEEvent
+#define TCSEEEvent
+
+class EEEvent
+{
+
+public:
+        // Particle and Lorentz vectors
+        Particle* Electron;
+        Particle Proton;
+        Particle Positron;
+        TLorentzVector vBeam;
+        TLorentzVector vRestProton;
+
+        int recem = 0;
+        int recep = 0;
+        int recp = 0;
+
+        // Kinematic variables
+        float MMassBeam;
+
+        // Lepton HTCC Nphe
+        float electron_1_Nphe = 0;
+        float electron_2_Nphe = 0;
+
+        // Lepton SF
+        float electron_1_SF = 0;
+        float electron_2_SF = 0;
+
+        // run number and trigger bit
+        float run;
+        int trigger_bit;
+
+        BGEvent()
+        {
+                vRestProton.SetPxPyPzE(0., 0., 0., mp);
+                vBeam.SetPxPyPzE(0., 0., ebeam, ebeam);
+                Electron = new Particle[2];
+        }
+
+        void Set_Particles(hipo::bank PART)
+        {
+
+                for (int i = 0; i < PART.getRows(); i++)
+                {
+
+                        int pid = PART.getInt("pid", i);
+                        float px = PART.getFloat("px", i);
+                        float py = PART.getFloat("py", i);
+                        float pz = PART.getFloat("pz", i);
+                        float beta = PART.getFloat("beta", i);
+                        int status = abs(PART.getInt("status", i));
+                        int charge = PART.getInt("charge", i);
+                        float chi2 = PART.getFloat("chi2pid", i);
+                        float vx = PART.getFloat("vx", i);
+                        float vy = PART.getFloat("vy", i);
+                        float vz = PART.getFloat("vz", i);
+                        float vt = PART.getFloat("vt", i);
+                        if (pid == -11)
+                        {
+                                Positron.Vector.SetXYZM(px, py, pz, mass_pion);
+                                Positron.index = i;
+                                Positron.pid = -11;
+                                Positron.beta = beta;
+                                Positron.status = status;
+                                Positron.chi2 = chi2;
+                                Positron.vertex.x = vx;
+                                Positron.vertex.y = vy;
+                                Positron.vertex.z = vz;
+                                Positron.vt = vt;
+                                recep++;
+                        }
+
+                        if (pid == 11)
+                        {
+
+                                if (status < 2000)
+                                        continue;
+                                
+                                if (recem>1)
+                                        continue;
+
+                                Electron[recem].Vector.SetXYZM(px, py, pz, me);
+                                Electron[recem].index = i;
+                                Electron[recem].pid = 11;
+                                Electron[recem].beta = beta;
+                                Electron[recem].status = status;
+                                Electron[recem].chi2 = chi2;
+                                Electron[recem].vertex.x = vx;
+                                Electron[recem].vertex.y = vy;
+                                Electron[recem].vertex.z = vz;
+                                Electron[recem].vt = vt;
+                                recem++;
+                        }
+
+                        if (pid == 2212)
+                        {
+                                Proton.Vector.SetXYZM(px, py, pz, mp);
+                                Proton.index = i;
+                                Proton.pid = 2212;
+                                Proton.beta = beta;
+                                Proton.status = status;
+                                Proton.chi2 = chi2;
+                                Proton.vertex.x = vx;
+                                Proton.vertex.y = vy;
+                                Proton.vertex.z = vz;
+                                Proton.vt = vt;
+                                recp++;
+                        }
+                }
+        }
+
+        void Compute_SF()
+        {
+                
+                electron_1_SF = ((Electron[0].Energy(ECAL, PCAL) + Electron[0].Energy(ECAL, ECIN) + Electron[0].Energy(ECAL, ECOUT))) / Electron[0].Vector.P();
+                electron_2_SF = ((Electron[1].Energy(ECAL, PCAL) + Electron[1].Energy(ECAL, ECIN) + Electron[1].Energy(ECAL, ECOUT))) / Electron[1].Vector.P();
+                //positron_SF = ((Positron.Energy(ECAL, PCAL) + Positron.Energy(ECAL, ECIN) + Positron.Energy(ECAL, ECOUT))) / Positron.Vector.P();
+        }
+
+        void Set_Nphe_HTCC()
+        {
+                electron_1_Nphe = Electron[0].nphe(15);
+                electron_2_Nphe = Electron[1].nphe(15);
+        }
+
+        void Apply_EC_Cuts(hipo::bank CALO)
+        {
+                Electron[0] = ApplyECcuts(Electron[0], CALO);
+                Electron[1] = ApplyECcuts(Electron[1], CALO);
+                //Positron = ApplyECcuts(Positron, CALO);
+                Proton = ApplyECcuts(Proton, CALO);
+        }
+
+        /*bool pass_EC_cut()
+        {
+                return (Electron.passEC && Positron.passEC && Proton.passEC && PionM.passEC);
+        }*/
+
+        void Associate_detector_resp(hipo::bank CHE, hipo::bank SCIN, hipo::bank CALO)
+        {
+                vector<Particle> Particles = {Positron, Electron[0], Electron[1], Proton};
+
+                CalorimeterResp Calo;
+                CheResp Che;
+                ScinResp Scin;
+                for (int i = 0; i < Particles.size(); i++)
+                {
+
+                        for (int c = 0; c < CHE.getRows(); c++)
+                        {
+                                int Chepindex = CHE.getInt("pindex", c);
+                                int Chedetector = CHE.getInt("detector", c);
+                                int Chesector = CHE.getInt("sector", c);
+                                float Chenphe = CHE.getFloat("nphe", c);
+                                float Chetime = CHE.getFloat("time", c);
+                                float Chechi2 = CHE.getFloat("chi2", c);
+                                float Chex = CHE.getFloat("x", c);
+                                float Chey = CHE.getFloat("y", c);
+                                float Chez = CHE.getFloat("z", c);
+
+                                if (Chepindex == (Particles[i].index))
+                                {
+                                        Che.detector = Chedetector;
+                                        Che.pindex = Chepindex;
+                                        Che.sector = Chesector;
+                                        Che.nphe = Chenphe;
+                                        Che.time = Chetime;
+                                        Che.chi2 = Chechi2;
+                                        Che.x = Chex;
+                                        Che.y = Chey;
+                                        Che.z = Chez;
+                                        Particles[i].Cherenkov.push_back(Che);
+                                }
+                        }
+                        for (int c = 0; c < SCIN.getRows(); c++)
+                        {
+                                int Scindetector = SCIN.getInt("detector", c);
+                                int Scinpindex = SCIN.getInt("pindex", c);
+                                float Scintime = SCIN.getFloat("time", c);
+                                float Scinpath = SCIN.getFloat("path", c);
+                                float Scinenergy = SCIN.getFloat("energy", c);
+                                int Scinsector = SCIN.getInt("sector", c);
+
+                                if (Scinpindex == (Particles[i].index))
+                                {
+                                        Scin.detector = Scindetector;
+                                        Scin.pindex = Scinpindex;
+                                        Scin.t = Scintime;
+                                        Scin.path = Scinpath;
+                                        Scin.energy = Scinenergy;
+                                        Scin.sector = Scinsector;
+
+                                        if (Particles[i].Scintillator.energy < Scinenergy)
+                                        {
+                                                Particles[i].Scintillator = Scin;
+                                        };
+                                }
+                        }
+
+                        for (int c = 0; c < CALO.getRows(); c++)
+                        {
+                                int Calopindex = CALO.getInt("pindex", c);
+                                int Calosector = CALO.getInt("sector", c);
+                                int Calolayer = CALO.getInt("layer", c);
+                                int Calodetector = CALO.getInt("detector", c);
+                                float Caloenergy = CALO.getFloat("energy", c);
+                                float Calox = CALO.getFloat("x", c);
+                                float Caloy = CALO.getFloat("y", c);
+                                float Caloz = CALO.getFloat("z", c);
+                                float Calou = CALO.getFloat("lu", c);
+                                float Calov = CALO.getFloat("lv", c);
+                                float Calow = CALO.getFloat("lw", c);
+                                float Calodu = CALO.getFloat("du", c);
+                                float Calodv = CALO.getFloat("dv", c);
+                                float Calodw = CALO.getFloat("dw", c);
+                                float Calom2u = CALO.getFloat("m2u", c);
+                                float Calom2v = CALO.getFloat("m2v", c);
+                                float Calom2w = CALO.getFloat("m2w", c);
+                                float Calom3u = CALO.getFloat("m3u", c);
+                                float Calom3v = CALO.getFloat("m3v", c);
+                                float Calom3w = CALO.getFloat("m3w", c);
+
+                                if (Calopindex == (Particles[i].index))
+                                {
+                                        Calo.detector = Calodetector;
+                                        Calo.pindex = Calopindex;
+                                        Calo.sector = Calosector;
+                                        Calo.layer = Calolayer;
+                                        Calo.energy = Caloenergy;
+                                        Calo.x = Calox;
+                                        Calo.y = Caloy;
+                                        Calo.z = Caloz;
+                                        Calo.u = Calou;
+                                        Calo.v = Calov;
+                                        Calo.w = Calow;
+                                        Calo.du = Calodu;
+                                        Calo.dv = Calodv;
+                                        Calo.dw = Calodw;
+                                        Calo.m2u = Calom2u;
+                                        Calo.m2v = Calom2v;
+                                        Calo.m2w = Calom2w;
+                                        Calo.m3u = Calom3u;
+                                        Calo.m3v = Calom3v;
+                                        Calo.m3w = Calom3w;
+
+                                        Particles[i].Calorimeter.push_back(Calo);
+                                }
+                        }
+
+                        Positron = Particles[0];
+                        Electron[0] = Particles[1];
+                        Electron[1] = Particles[2];
+                        Proton = Particles[3];
+
+                }
+        };
+};
+#endif
