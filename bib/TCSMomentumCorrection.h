a29e1907d173bef87ec355ae0f06823bbfe9899b
CHATAGNON Pierre <pc281174@dphnmct132.extra.cea.fr>
2024-11-19 12:54:08 +0100

    correct the proton correction v2

--- bib/TCSMomentumCorrection.h
+++ bib/TCSMomentumCorrection.h
@@ -204,9 +204,9 @@ public:
 
 		// Electron->Vector.SetXYZM(Electron->Vector.Px()/corr_factor_electron, Electron->Vector.Py()/corr_factor_electron , Electron->Vector.Pz()/corr_factor_electron, me);
 		// Positron->Vector.SetXYZM(Positron->Vector.Px()/corr_factor_positron, Positron->Vector.Py()/corr_factor_positron, Positron->Vector.Pz()/corr_factor_positron, me);
-		double pPxCorr = corr_factor_proton * sin(Proton->Vector.Theta()) * cos(Proton->Vector.Phi());
-		double pPyCorr = corr_factor_proton * sin(Proton->Vector.Theta()) * sin(Proton->Vector.Phi());
-		double pPzCorr = corr_factor_proton * cos(Proton->Vector.Theta());
+		double pPxCorr = corr_factor_proton * Proton->Vector.P() * sin(Proton->Vector.Theta()) * cos(Proton->Vector.Phi());
+		double pPyCorr = corr_factor_proton * Proton->Vector.P() * sin(Proton->Vector.Theta()) * sin(Proton->Vector.Phi());
+		double pPzCorr = corr_factor_proton * Proton->Vector.P() * cos(Proton->Vector.Theta());
 		Proton->Vector.SetXYZM(pPxCorr, pPyCorr, pPzCorr, mp);
 	}
 };
