80c3000223ffc2b917e44fc3f69a87cdfc9a9671
PChatagnon <pierre.chata@orange.fr>
2025-04-30 16:57:30 +0200

    fix angle

--- analysis_muCLAS12.C
+++ analysis_muCLAS12.C
@@ -356,7 +356,7 @@ int analysis_muCLAS12()
 			outVars["W"] = ev.W;
 
 
-			ThetaPhi cm = funcCM(ev.mu_minus.Vector, ev.mu_plus.Vector, ev.vMissing);
+			ThetaPhi cm = funcCM(ev.mu_plus.Vector, ev.mu_minus.Vector, ev.vMissing);
 			outVars["phi"] = cm.phi;
 			outVars["theta"] = cm.theta;
 
